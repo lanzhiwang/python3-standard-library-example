7b9d3ba8334f2bdcd0f387c682b43a6b7a54c1da
lanzhiwang <hzhilamp@163.com>
2020-02-22 19:31:25 +0800

    python gil

--- source/05_threading/evaluating_performances.py
+++ source/05_threading/evaluating_performances.py
@@ -1,66 +1,129 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
-"""
-"""
-
-#end_pymotw_header
 from threading import Thread
+import multiprocessing
 
-def function_to_run():
-    pass
+
+def show_results(func_name, results):
+    print("%-23s %4.6f seconds" % (func_name, results))
+
+
+class process_object(multiprocessing.Process):
+    def __init__(self, func):
+        multiprocessing.Process.__init__(self)
+        self.func = func
+
+    def run(self):
+        self.func()
+
+
+def process(num_process, func):
+    funcs = []
+    for i in range(int(num_process)):
+        funcs.append(process_object(func))
+    for i in funcs:
+        i.start()
+    for i in funcs:
+        i.join()
 
 
 class threads_object(Thread):
-	def run(self):
-		function_to_run()
+    def __init__(self,
+                 group=None,
+                 target=None,
+                 name=None,
+                 args=(),
+                 kwargs=None,
+                 *,
+                 daemon=None):
+        super().__init__(group=group, target=target, name=name, daemon=daemon)
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        self.args[0]()
 
 
-class nothreads_object(object):
-	def run(self):
-		function_to_run()
+def threaded(num_threads, func):
+    funcs = []
+    for i in range(int(num_threads)):
+        funcs.append(threads_object(args=(func, )))
+    for i in funcs:
+        i.start()
+    for i in funcs:
+        i.join()
 
 
-def non_threaded(num_iter):
-	funcs = []
-	for i in range(int(num_iter)):
-		funcs.append(nothreads_object())
-	for i in funcs:
-		i.run()
- 
+class non_object(object):
+    def __init__(self, func):
+        self.func = func
 
-def threaded(num_threads):
-	funcs = []
-	for i in range(int(num_threads)):
-		funcs.append(threads_object())
-	for i in funcs:
-		i.start()
-	for i in funcs:
-		i.join()
- 
+    def run(self):
+        self.func()
 
-def show_results(func_name, results):
-	print ("%-23s %4.6f seconds"\
-               % (func_name, results))
- 
+
+def non(num_iter, func):
+    funcs = []
+    for i in range(int(num_iter)):
+        funcs.append(non_object(func))
+    for i in funcs:
+        i.run()
+
+
+def function_to_run1():
+    pass
+
+
+def function_to_run2():
+    a, b = 1, 1
+    for _ in range(10000):
+        a, b = b, a + b
+
+
+def function_to_run3():
+    from urllib import request
+    response = request.urlopen('http://www.baidu.com/')
+    response.read(1024)
+
+
 if __name__ == "__main__":
-	 import sys
-	 from timeit import Timer
- 
-	 repeat = 100
-	 number = 1
-	 num_threads = [1, 2, 4, 8]
+    import sys
+    from timeit import Timer
 
- 	 print ('Starting tests')
-	 for i in num_threads:
-		 t = Timer("non_threaded(%s)" % i, "from __main__ import non_threaded")
-		 best_result = min(t.repeat(repeat=repeat, number=number))
-		 show_results("non_threaded (%s iters)" % i, best_result)
- 
-		 t = Timer("threaded(%s)" % i, "from __main__ import threaded")
-		 best_result = min(t.repeat(repeat=repeat, number=number))
-		 show_results("threaded (%s threads)" % i, best_result)
+    repeat = 10
+    number = 1
+    num = [1, 2, 4, 8]
 
-print ('Iterations complete') 
+    for func in [function_to_run1, function_to_run2, function_to_run3]:
+        for i in num:
+            t = Timer("non(%s, %s)" % (i, func.__name__),
+                      "from __main__ import non",
+                      globals=globals())
+            best_result = min(t.repeat(repeat=repeat, number=number))
+            show_results("non (%s iters)" % i, best_result)
+
+            t = Timer("threaded(%s, %s)" % (i, func.__name__),
+                      "from __main__ import threaded",
+                      globals=globals())
+            best_result = min(t.repeat(repeat=repeat, number=number))
+            show_results("threaded (%s threads)" % i, best_result)
+
+            t = Timer("process(%s, %s)" % (i, func.__name__),
+                      "from __main__ import process",
+                      globals=globals())
+            best_result = min(t.repeat(repeat=repeat, number=number))
+            show_results("process (%s process)" % i, best_result)
+        print('Iterations complete')
+"""
+non (1 iters)           0.000001 seconds
+threaded (1 threads)    0.000069 seconds
+process (1 process)     0.002885 seconds
+non (2 iters)           0.000001 seconds
+threaded (2 threads)    0.000115 seconds
+process (2 process)     0.003724 seconds
+non (4 iters)           0.000002 seconds
+threaded (4 threads)    0.000189 seconds
+process (4 process)     0.004960 seconds
+non (8 iters)           0.000003 seconds
+threaded (8 threads)    0.000364 seconds
+process (8 process)     0.007469 seconds
+
+"""
