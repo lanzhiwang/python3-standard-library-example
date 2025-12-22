d9c23788b6ba3729ed2197854847929db31cb426
lanzhiwang <hzhilamp@163.com>
2025-12-22 16:56:38 +0800

    24_multiprocessing

--- source/24_multiprocessing/spawn_a_process.py
+++ source/24_multiprocessing/spawn_a_process.py
@@ -22,9 +22,11 @@ if __name__ == "__main__":
         p.join()
 
 """
+root@dff419eed0f6:/python3-standard-library-example/source/24_multiprocessing# python 01_spawn_a_process.py
 called function in process: 0
 called function in process: 1
 called function in process: 2
 called function in process: 3
 called function in process: 4
+root@dff419eed0f6:/python3-standard-library-example/source/24_multiprocessing#
 """
